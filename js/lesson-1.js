0cf82da94c05924a6e7006e893ea7266f3f6a1b9
Oleksandr44 <tkachenkooleksandr.it@gmail.com>
2024-01-24 00:58:20 +0200

    Модуль 1

---
+++ js/lesson-1.js
@@ -0,0 +1,83 @@
+// a = (100 + 2) + "30";
+// const a = 5;
+
+// console.log(a);
+
+// let b = 33;
+
+// console.log(b);
+
+// b = 44;
+
+// console.log(b)
+
+// const userAge = 15;
+// const userName = "Dims";
+// const isUserAdmin = false;
+
+// // typeof - це оператор, за допомогою якого можна перевірити тип даних якогось значення або змінної
+
+// console.log(typeof userAge); //?"number"
+// console.log(typeof userName); //? "string"
+// console.log(typeof isUserAdmin); //? "boolean"
+// console.log(typeof undefined); //? "underfined"
+// console.log(typeof null); //? "object"
+// console.log(NaN); //? "not a number" === "NaN"
+// console.log(typeof NaN); //? "number"
+
+/* Арифметичні операції */
+
+// const a = 10;
+// const b = 5;
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+// console.log(a ** b);
+
+/* Комбіновані оператори */
+
+// let students = 100;
+
+// // students = students + 50; //? "дорівнює 150, до 100 студентів додали 50"
+// // students += 50; //? "дорівнює теж 150, ми до самого себе додаємо 50"
+
+// console.log(students);
+
+/* Пріоритет операторів*/
+
+// const result = 108 + 223 - 2 * 5;
+
+// console.log(result);
+
+
+// const firstName = "Chelsy";
+// const lastName = "Emerald";
+
+// const fullName = `${firstName} ${lastName}`;
+// console.log(fullName);
+
+// const quantity = 15;
+// const orderMsg = `You ordered ${quantity} bots`;
+
+// console.log(orderMsg);
+
+// const message = "This string is 28 chars long"
+
+// console.log(message.length);
+
+// /**
+//  * Склади фразу за допомогою шаблонних рядків
+//  * A has B bots in stock, де A, B - змінні вставлені в рядок.
+//  */
+
+// const companyName = "Cyberdyne Systems";
+// const repairBots = 150;
+// const defenceBots = 50;
+
+// const msg = `${companyName} has ${repairBots + defenceBots} bots in stock`;
+
+// console.log(msg);
+
